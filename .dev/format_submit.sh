90dea08577a04e346468cbf341c894d36d96f665
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-04-23 04:26:07 -0400

    DEV: Added file submission script

---
+++ .dev/format_submit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# NOTE: Run this script/command from the .dev directory
+
+chmod 755 ../report/cse-505-project/file_sub.py
+
+../report/cse-505-project/file_sub.py -e project -e repository -e phase2 -f <file>
+
