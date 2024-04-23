d2534bd66cf5506043d2c947ff54aa68e2f68a1a
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-04-23 02:55:29 -0400

    DOC: Added comment

--- .dev/measure_code_base.sh
+++ .dev/measure_code_base.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Measure size of code base
+# 
+# Found from the following link: https://stackoverflow.com/a/48254241
+
 # TODO: Write instructions and details later
 
 # NOTE: This script/command must be run from the root of the project
