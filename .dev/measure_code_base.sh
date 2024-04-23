5545bfeb97dd0bbefc240c29b8a8d8addec81ff3
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-04-23 02:54:25 -0400

    DEV: Added way of measureing code base

---
+++ .dev/measure_code_base.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# TODO: Write instructions and details later
+
+# NOTE: This script/command must be run from the root of the project
+
+# Measure the code base
+git ls-files | xargs wc -l
