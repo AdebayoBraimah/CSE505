aa329177567b3393d4dfb0235763c045e86e03c5
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-04-22 23:09:40 -0400

    API: Added functionality for scheduling, updated clingo interface

--- src/clapi/clapi.py
+++ src/clapi/clapi.py
@@ -1,13 +1,31 @@
 """Clingo API module for organizing and processing course data.
-"""
-import json
 
-from typing import List,Union
+.. autosummary::
+    :nosignatures:
+
+    process_course_data_clingo
+    append_rules
+    query_clingo
+"""
+
+import json
+import sys
+import shlex
+import subprocess
+
+from typing import List, Union
 
 from src.kg.knowledge_graph import KnowledgeBase, KnowledgeGraph
+from src.utils.util import DependencyError, check_dependencies
 
-def process_course_data_clingo(file_path: Union[KnowledgeBase,KnowledgeGraph,str], file_name: str = None) -> str:
-    """Converts a JSON file data to a Clingo .
+
+def process_course_data_clingo(
+    file_path: Union[KnowledgeBase, KnowledgeGraph, str], file_name: str = None
+) -> Union[str, KnowledgeBase, KnowledgeGraph]:
+    """Converts a JSON file data to a Clingo.
+
+    NOTE:
+        - If a ``KnowledgeBase`` or ``KnowledgeGraph`` object is passed, then the Clingo filepath is updated in the object.
 
     Args:
         file_path: Input JSON file (or ``KnowledgeBase`` or ``KnowledgeGraph`` object) to be converted to ERGO file.
@@ -17,14 +35,16 @@ def process_course_data_clingo(file_path: Union[KnowledgeBase,KnowledgeGraph,str
         Output clingo knowledge base/graph file path.
     """
     # Load JSON data from the file
-    if (isinstance(file_path, KnowledgeBase)) or (isinstance(file_path, KnowledgeGraph)):
-        kg: Union[KnowledgeBase,KnowledgeGraph] = file_path
+    if (isinstance(file_path, KnowledgeBase)) or (
+        isinstance(file_path, KnowledgeGraph)
+    ):
+        kg: Union[KnowledgeBase, KnowledgeGraph] = file_path
         file_path: str = kg.json
-     
+
     if output_file is None:
         output_file = file_path.replace(".json", ".lp")
 
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         data = json.load(file)
 
     courses_list = []
@@ -35,10 +55,14 @@ def process_course_data_clingo(file_path: Union[KnowledgeBase,KnowledgeGraph,str
         # Extract course information
         course_name = course_code  # Simplified as the key
         try:
-            credits = int(details['Credits']) if isinstance(details['Credits'], (float, int)) else 3  # Assuming default 3 if not clear
+            credits = (
+                int(details["Credits"])
+                if isinstance(details["Credits"], (float, int))
+                else 3
+            )  # Assuming default 3 if not clear
         except ValueError:
             continue
-        prerequisites = details.get('Prerequisites', [])
+        prerequisites = details.get("Prerequisites", [])
 
         # Print course information
         # print(f"course({course_name.lower()}, {credits}).")
@@ -52,15 +76,21 @@ def process_course_data_clingo(file_path: Union[KnowledgeBase,KnowledgeGraph,str
                     # Format and clean prerequisite course code
                     prereq_code = prereq.replace(" ", "").upper()
                     # print(f"prerequisite({course_name.lower()}, {prereq_code.lower()}).")
-                    if 'cs' in prereq_code.lower():
+                    if "cs" in prereq_code.lower():
                         if prereq_code.lower() < course_name.lower():
-                            prerequisites_list.append(f"prerequisite({course_name.lower()}, {prereq_code.lower()}).")
+                            prerequisites_list.append(
+                                f"prerequisite({course_name.lower()}, {prereq_code.lower()})."
+                            )
                         # prerequisites_list.append(f"prerequisite({course_name.lower()}, {prereq_code.lower()}).")
-    
+
     output = courses_list + prerequisites_list
     _write_list_to_file(output, file_name)
 
+    if kg:
+        kg.lp = file_name
+
     return output
+
 
 def _write_list_to_file(data_list, filename):
     with open(filename, "w") as file:
@@ -81,18 +111,57 @@ def append_rules(file_list: List[str], output_file: str) -> str:
     """
     try:
         # Open the output file in write mode (this will overwrite existing file)
-        with open(output_file, 'w') as outfile:
+        with open(output_file, "w") as outfile:
             # Iterate over the list of files
             for file_name in file_list:
                 # Open each file in read mode
-                with open(file_name, 'r') as infile:
+                with open(file_name, "r") as infile:
                     # Read the content of the file
                     content = infile.read()
                     # Write the content to the output file
                     outfile.write(content)
                     # Optionally add a newline between contents of different files
-                    outfile.write('\n')
+                    outfile.write("\n")
         # print(f"Contents appended successfully into {output_file}.")
         return output_file
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def query_clingo(
+    knowledge: Union[KnowledgeBase, KnowledgeGraph, str], verbose: bool = False
+) -> str:
+    """Queries a Clingo knowledge base/graph file using a given query.
+
+    WARNING:
+        - This function assumes that ``clingo`` is installed and is accessible via the system path variable.
+
+    NOTE:
+        - The query must be included in the Clingo file.
+
+    Args:
+        knowledge: Input Clingo knowledge base/graph file (or ``KnowledgeBase`` or ``KnowledgeGraph`` object) to be queried.
+        verbose: Prints verbose output if set to ``True``. Defaults to ``False``.
+
+    Returns:
+        Query results.
+    """
+    # Check if Clingo is installed
+    try:
+        check_dependencies(dependencies=("clingo",))
+    except DependencyError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    # Set verbose option if specified
+    if verbose:
+        cmd_opt: str = "-V"
+
+    # Start Clingo session
+    proc = subprocess.Popen(
+        shlex.split(s=f"clingo {cmd_opt} {knowledge}"), stdout=subprocess.PIPE
+    )
+
+    output = proc.stdout.read().decode("utf-8")
+
+    return output
--- src/schedule.py
+++ src/schedule.py
@@ -1,18 +1,185 @@
-"""_summary_
+"""Stony Brook University course scheduling module.
+
+At the moment, this module only supports CSE majors.
+
+.. autosummary::
+    :nosignatures:
+
+    create_schedule
+    create_schedule_ergo
+    create_schedule_clingo
+
+.. autochart:: src.schedule.create_schedule
 """
 
-from typing import List, Union
+import os
+from typing import Union
 
-from src.kg.knowledge_graph import KnowledgeBase, KnowledgeGraph
-from src.ergoai import json_to_ergo, query_ergoai
+from src import RESROURCEDIR
+from src.kg.knowledge_graph import KnowledgeBase, KnowledgeGraph, scrape_sbu_solar
+from src.ergoai.ergoai import json_to_ergo, query_ergoai
+from src.clapi.clapi import process_course_data_clingo, append_rules, query_clingo
 
-def create_schedule(courses: Union[str,KnowledgeBase,KnowledgeGraph], major: str, method: str = "clingo"):
-    """_summary_
+
+def create_schedule(
+    courses: Union[str, KnowledgeBase, KnowledgeGraph] = None,
+    major: str = "CSE",
+    query: str = None,
+    method: str = "clingo",
+    verbose: bool = False,
+    lp_file: Union[str, KnowledgeBase, KnowledgeGraph] = None,
+) -> str:
+    """Creates a schedule based on the given courses, major, and query.
+
+    Args:
+        courses: Input knowledge base/graph file or URL of Stony Brook University courses.
+        major: Major, represented as a three-letter code (e.g. "CSE").
+        query: Query string (in the case of ``ERFO``) or file (in the case of ``Clingo``) to be used for scheduling.
+        method: Method to be used for scheduling. Options are "ergo" and "clingo". Defaults to ``clingo``.
+        verbose: If True, then additional information is printed to the console. Defaults to False.
+        lp_file: Input ``clingo`` knowledge base/graph file of Stony Brook University courses. If provided, then the ``courses``, ``major``, and ``query`` arguments are ignored.
+
+    Raises:
+        ValueError: Arises when an invalid argument for ``method`` is provided.
+
+    Returns:
+        Schedule results.
     """
-    pass
+    if method.lower() == "ergo":
+        results: str = create_schedule_ergo(courses=courses, major=major, query=query)
+    elif method.lower() == "clingo":
+        results: str = create_schedule_clingo(
+            courses=courses,
+            major=major,
+            query_file=query,
+            verbose=verbose,
+            lp_file=lp_file,
+        )
+    else:
+        raise ValueError(f"Invalid method: {method}")
+    return results
 
-def create_schedule_ergo(courses: Union[str,KnowledgeBase,KnowledgeGraph], major: str):
-    pass
 
-def create_schedule_clingo(courses: Union[str,KnowledgeBase,KnowledgeGraph], major: str):
-    pass
+def create_schedule_ergo(
+    courses: Union[str, KnowledgeBase, KnowledgeGraph], major: str, query: str
+) -> str:
+    """Creates a schedule based on the given courses, major, and query using ErgoAI.
+
+    Args:
+        courses: Input knowledge base/graph file or URL of Stony Brook University courses.
+        major: Major, represented as a three-letter code (e.g. "CSE").
+        query: Query string to be used for scheduling.
+
+    Raises:
+        ValueError: Arises when an invalid argument for ``courses`` or ``major`` is provided.
+
+    Returns:
+        Schedule results.
+    """
+    if isinstance(courses, str):
+        if (courses.beginwith("http")) and major:
+            kg: KnowledgeGraph = scrape_sbu_solar(
+                url=courses,
+                major_three_letter_code=major,
+                output_filename=os.path.join(
+                    RESROURCEDIR, f"{major.lower()}.courses.json"
+                ),
+            )
+        elif courses.endswith(".json"):
+            kg: KnowledgeGraph = KnowledgeGraph(json=courses)
+        elif courses.endswith(".ergo"):
+            kg: KnowledgeGraph = KnowledgeGraph(ergo=courses)
+        else:
+            raise ValueError(f"Invalid input file: {courses}")
+    elif isinstance(courses, KnowledgeBase) or isinstance(courses, KnowledgeGraph):
+        kg: Union[KnowledgeBase, KnowledgeGraph] = courses
+    else:
+        raise ValueError(f"Invalid input type: {type(courses)}")
+
+    if (
+        (courses.endswith(".json"))
+        or (courses.beginwith("http"))
+        or (kg.json)
+        or (not kg.ergo)
+    ):
+        _: str = json_to_ergo(
+            json_file=kg,
+            output_file=os.path.join(RESROURCEDIR, f"{major.lower()}.courses.ergo"),
+        )
+
+    results: str = query_ergoai(kg=kg, query=query)
+
+    return results
+
+
+def create_schedule_clingo(
+    courses: Union[str, KnowledgeBase, KnowledgeGraph] = None,
+    major: str = "CSE",
+    query_file: str = None,
+    verbose: bool = False,
+    lp_file: Union[str, KnowledgeBase, KnowledgeGraph] = None,
+) -> str:
+    """Creates a schedule based on the given courses, major, and query using Clingo.
+
+    Args:
+        courses: Input knowledge base/graph file or URL of Stony Brook University courses.
+        major: Major, represented as a three-letter code (e.g. "CSE").
+        query_file: Query file to be used for scheduling.
+        verbose: If True, then additional information is printed to the console. Defaults to False.
+        lp_file: Input knowledge base/graph file of Stony Brook University courses. If provided, then the ``courses``, ``major``, and ``query_file`` arguments are ignored.
+
+    Raises:
+        ValueError: Arises when an invalid argument for ``courses`` or ``major`` is provided.
+
+    Returns:
+        Schedule results.
+    """
+    if lp_file:
+        if isinstance(lp_file, str):
+            if lp_file.endswith(".lp"):
+                kg: KnowledgeGraph = KnowledgeGraph(lp=lp_file)
+            else:
+                raise ValueError(f"Invalid input file: {lp_file}")
+        elif isinstance(lp_file, KnowledgeBase) or isinstance(lp_file, KnowledgeGraph):
+            kg: Union[KnowledgeBase, KnowledgeGraph] = lp_file
+        else:
+            raise ValueError(f"Invalid input type: {type(lp_file)}")
+    elif isinstance(courses, str):
+        if (courses.beginwith("http")) and major:
+            kg: KnowledgeGraph = scrape_sbu_solar(
+                url=courses,
+                major_three_letter_code=major,
+                output_filename=os.path.join(
+                    RESROURCEDIR, f"{major.lower()}.courses.json"
+                ),
+            )
+        elif courses.endswith(".json"):
+            kg: KnowledgeGraph = KnowledgeGraph(json=courses)
+        elif courses.endswith(".lp"):
+            kg: KnowledgeGraph = KnowledgeGraph(lp=courses)
+        else:
+            raise ValueError(f"Invalid input file: {courses}")
+    elif isinstance(courses, KnowledgeBase) or isinstance(courses, KnowledgeGraph):
+        kg: Union[KnowledgeBase, KnowledgeGraph] = courses
+    else:
+        raise ValueError(f"Invalid input type: {type(courses)}")
+
+    if (
+        (courses.endswith(".json"))
+        or (courses.beginwith("http"))
+        or (kg.json)
+        or (not kg.lp)
+        or (not lp_file)
+    ):
+        _: str = process_course_data_clingo(file_path=kg, file_name=None)
+
+    if lp_file:
+        results: str = query_clingo(kg=kg.lp, verbose=verbose)
+    else:
+        output: str = append_rules(
+            kg=[kg.lp, query_file],
+            output_file=os.path.join(RESROURCEDIR, f"{major.lower()}.schedule.lp"),
+        )
+        results: str = query_clingo(kg=output, verbose=verbose)
+
+    return results
