da707246664718d37132f3170fd067a991e8f3a3
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-05-01 03:39:46 -0400

    API: Downloaded course info for CSE reqs

---
+++ download_courses.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+_PKG_PATH = "/Users/adebayobraimah/Desktop/projects/CSE505"
+
+sys.path.append(_PKG_PATH)
+
+from src import RESROURCEDIR
+from src.kg.knowledge_graph import scrape_sbu_solar, KnowledgeGraph
+from src.ergoai.ergoai import json_to_ergo
+
+url: str = (
+    "https://prod.ps.stonybrook.edu/psc/csprodg/EMPLOYEE/CAMP/c/COMMUNITY_ACCESS.SSS_BROWSE_CATLG.GBL?"
+)
+
+
+def main(major: str) -> None:
+    outname: str = os.path.join(RESROURCEDIR, f"{major}")
+
+    if not (os.path.exists(f"{outname}_courses.json")):
+        kg = scrape_sbu_solar(
+            url,
+            major_three_letter_code=major,
+            wait_time=10,
+            headless=True,
+            verbose=True,
+        )
+        kg.df.to_csv(f"{outname}_courses.csv", index=True)
+        kg.df.to_json(f"{outname}_courses.json", orient="index", indent=4)
+
+    if not (os.path.exists(f"{outname}_courses.ergo")):
+        json_to_ergo(json_file=f"{outname}_courses.json")
+
+    return None
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: download_courses.py <major>")
+        sys.exit(1)
+
+    main(sys.argv[1])
---
+++ download_courses.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+download_courses=$(realpath download_courses.py)
+
+# Array of course majors
+courses=(
+    "cse"
+    "ams"
+    "bio"
+    "phy"
+    "ast"
+    "che"
+    "geo"
+)
+
+# Create job file
+job_file="job.txt"
+
+# Remove job file if it exists
+if [[ -f ${job_file} ]]; then
+    rm ${job_file}
+fi
+
+# Loop through each course major
+for course in "${courses[@]}"; do
+    # ${download_courses} ${course}
+    echo "${download_courses} ${course}" >> ${job_file}
+done
+
+# Run job file
+parallel -j 4 < ${job_file}
