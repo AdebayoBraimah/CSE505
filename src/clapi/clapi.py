b2fc3e1eea47ed57fdbada554d51e6114179b3a1
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-05-05 05:53:52 -0400

    API: Added support for repeatable courses

--- src/clapi/clapi.py
+++ src/clapi/clapi.py
@@ -8,14 +8,16 @@
     append_rules
     query_clingo
     process_honors_courses
+    process_repeatable_courses
 """
 
+import re
 import json
 import sys
 import shlex
 import subprocess
 
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, Iterable, List, Union, Tuple
 
 from src.kg.knowledge_graph import KnowledgeBase, KnowledgeGraph
 from src.utils.util import DependencyError, check_dependencies
@@ -26,7 +28,9 @@ from src.utils.util import DependencyError, check_dependencies
 
 
 def process_course_data_clingo(
-    json_file: Union[KnowledgeBase, KnowledgeGraph, str], output_file: str = None
+    json_file: Union[KnowledgeBase, KnowledgeGraph, str],
+    output_file: str = None,
+    repeatable_courses: List[Tuple[str, str, str]] = None,
 ) -> str:
     """Converts a JSON file data to a Clingo.
     This function processes course data from a JSON file and writes it to a Clingo file in the form:
@@ -49,6 +53,7 @@ def process_course_data_clingo(
     Args:
         json_file: Input JSON file (or ``KnowledgeBase`` or ``KnowledgeGraph`` object) to be converted to Clingo.
         output_file: Output filename. If not specified, then a new file of the same name is created, with an '.lp' file extension. Defaults to None.
+        repeatable_courses: List of tuples of other courses that are repeatable. Each tuple should have exactly 3 elements: course_id, times_repeatable, max_credits. Defaults to None.
 
     Returns:
         Output Clingo knowledge base file path.
@@ -122,8 +127,29 @@ def process_course_data_clingo(
                         f':- course({course_id.lower()}, {credits}, "{career}", {offered_spring1}, {offered_fall1}, {offered_spring2}, {offered_fall2}), \n   not course({coreq.lower()}, _, "{career}", {offered_spring1}, {offered_fall1}, {offered_spring2}, {offered_fall2}). \n'
                     )
 
+    # Process honors courses
+    honors: List[str] = process_honors_courses(file_path=json_file)
+
+    if honors:
+        honors.insert(0, "\n% Honors Courses")
+
+    # Process repeatable courses
+    repeatable: List[str] = process_repeatable_courses(
+        json_file=json_file, other_courses=repeatable_courses
+    )
+
+    if repeatable:
+        repeatable.insert(0, "\n% Repeatable Courses")
+
     # Write predicates and rules to file
-    output = ["% Course predicates"] + predicates + ["\n% Course Rules \n"] + rules
+    output = (
+        ["% Course predicates"]
+        + predicates
+        + honors
+        + repeatable
+        + ["\n% Course Rules \n"]
+        + rules
+    )
     _write_list_to_file(output, output_file)
 
     if kg:
@@ -241,6 +267,15 @@ def translate_range(input_string: str) -> str:
 
 
 def _write_list_to_file(data_list: List[str], filename: str) -> None:
+    """Writes a list of data (strings) to a file.
+
+    Args:
+        data_list: Input list of data to be written to the file.
+        filename: Output filename.
+
+    Returns:
+        None
+    """
     with open(filename, "w") as file:
         for item in data_list:
             file.write(item + "\n")
@@ -318,18 +353,20 @@ def query_clingo(
     return output
 
 
-def process_honors_courses(file_path: str, outfile: str = None) -> None:
-    """Processes (CSE) honors courses from a JSON file and writes them to a clingo file in the form: ``cse_honors(course_id)``.
+def process_honors_courses(file_path: str) -> List[str]:
+    """Processes honors courses from a JSON file and writes them to a list in the form: ``honors(course_id)``.
+
+    NOTE:
+        - Mainly intended for use with SBU CSE courses.
 
     Usage example:
         >>> process_honors_courses(file_path="cse_courses.json")
 
     Args:
         file_path: Path to the JSON file containing course data.
-        outfile: Output filename. If not specified, then the output file will just have '_honors.lp' appended to the input filename. Defaults to None.
 
     Returns:
-        None.
+        List of honors courses.
     """
     # Load the JSON data from the file
     with open(file_path, "r") as file:
@@ -337,16 +374,136 @@ def process_honors_courses(file_path: str, outfile: str = None) -> None:
 
     course_list: List[str] = []
 
-    if outfile is None:
-        outfile = file_path.replace(".json", "_honors.lp")
-
     # Iterate over each course
     for course_id, course_details in data.items():
         # Check if 'honors' is in the description
-        if "honors" in course_details.get("Description", "").lower():
-            course_list.append(f"cse_honors({course_id}).")
+        if "honors" in course_details.get("CourseTitle", "").lower():
+            course_list.append(f"honors({course_id}).")
+        elif "honors" in course_details.get("Description", "").lower():
+            course_list.append(f"honors({course_id}).")
 
-    # Write the list to a file
-    _write_list_to_file(data_list=course_list, filename=outfile)
+    return course_list
 
-    return None
+
+def process_repeatable_courses(
+    json_file: str, other_courses: Iterable[Tuple[str, str, str]] = None
+) -> List[str]:
+    """Processes repeatable courses from a JSON file and writes them to a list in the form: ``repeatable(course_id, times_repeatable, max_credits)``.
+
+    NOTE:
+        - The input JSON file should contain course data.
+        - The repeatable courses are identified based on the course description.
+        - The function also accepts a list of other courses that are repeatable.
+
+    Usage example:
+        >>> process_repeatable_courses(json_file="course_data.json", other_courses=[("cse593", "_", "_")]
+        ['repeatable(cse390, 2, _).',
+         'repeatable(cse391, 2, _).',
+         'repeatable(cse392, 2, _).',
+         'repeatable(cse393, 2, _).',
+         'repeatable(cse394, 2, _).',
+         'repeatable(cse475, 2, _).',
+         'repeatable(cse488, _, 12).',
+         .
+         .
+         .
+         'repeatable(cse593, _, _).',
+         .
+         .
+         .
+         'repeatable(cse693, 2, _).']
+
+    Args:
+        json_file: Input JSON file containing course data.
+        other_courses: List of tuples of other courses that are repeatable. Each tuple should have exactly 3 elements: course_id, times_repeatable, max_credits. Defaults to None.
+
+    Raises:
+        ValueError: If each course tuple does not have exactly 3 elements, or if any of the 2nd or 3rd elements of the tuple do not contain an integer or the string "_".
+
+    Returns:
+        List of repeatable courses.
+    """
+    with open(json_file, "r") as file:
+        data: Dict[Dict[str, Any]] = json.load(file)
+
+    atoms: List[str] = []
+
+    # Add other repeatable courses
+    if other_courses is not None:
+        for course_info in other_courses:
+            if not course_info[0].upper() in data.keys():
+                print(f"Course {course_info[0]} not found in the JSON file.")
+                continue
+            if (
+                (len(course_info) != 3)
+                or (not _check_repeatable_input(course_info[1]))
+                or (not _check_repeatable_input(course_info[2]))
+            ):
+                raise ValueError("Each course tuple should have exactly 3 elements.")
+            atom = f"repeatable({course_info[0]}, {course_info[1]}, {course_info[2]})."
+            atoms.append(atom)
+
+    for course_id, course_info in data.items():
+        description = course_info["Description"].lower()
+        if ("repeat" in description) and (not course_id in atoms):
+            times = "_"
+            max_credits = "_"
+            if "more than twice" in description:
+                times = 2
+            elif "repeated once" in description:
+                times = 2
+            elif "repeated twice" in description:
+                times = 3
+            elif ("credits" in description.lower()) and (
+                _extract_credits(description) is not None
+            ):
+                max_credits = _extract_credits(description)
+            else:
+                # NOTE: Uncommenting the line below will set the default value of times to 2.
+                #   This behavior may not be desired in all cases.
+                # times: int = 2
+                continue
+            atom: str = f"repeatable({course_id.lower()}, {times}, {max_credits})."
+            atoms.append(atom)
+
+    # Sort the atoms
+    atoms: List[str] = sorted(atoms)
+    return atoms
+
+
+def _extract_credits(description: str) -> int:
+    """Helper function that extracts the number of credits from a course description.
+
+    Args:
+        description: Course description string.
+
+    Returns:
+        Number of credits.
+    """
+    # Search for one or more digits followed by the word 'credits'
+    match = re.search(r"(\d+) credits", description)
+    if match:
+        # Return the number found
+        return int(match.group(1))
+    else:
+        return None
+
+
+def _check_repeatable_input(course_info: str) -> bool:
+    """Helper function that checks if the input for repeatable courses is valid.
+    Valid input is either an integer or the string "_".
+
+    Args:
+        course_info: Input course information.
+
+    Returns:
+        True if the input is valid, False otherwise.
+    """
+    try:
+        int(course_info)
+        return True
+    except ValueError:
+        if course_info == "_":
+            return True
+        else:
+            return False
