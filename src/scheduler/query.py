099502aa0b7d6a4c82e7e1103aae965e13c99ff6
AdebayoBraimah <adebayo.braimah@gmail.com>
2024-05-09 09:50:03 -0700

    API & DOC: Updated function to take multiple input files

--- src/scheduler/query.py
+++ src/scheduler/query.py
@@ -6,7 +6,7 @@
     query
 """
 
-from typing import Union
+from typing import Tuple, Union
 
 from src.clapi.clapi import query_clingo
 from src.ergoai.ergoai import query_ergoai
@@ -20,19 +20,19 @@ def query(
     num_models: int = None,
     configuration: str = "handy",
     parallel_mode: int = None,
-    query: str = None,
+    query: Union[str, Tuple[str]] = None,
 ) -> str:
     """Executes queries for some knowledge base.
 
-    WARNING:
+    DANGER:
         - ErgoAI queries are not fully supported.
 
     Usage example:
         >>> from src.scheduler.query import query
-        >>> result = query(knowledge="cse_courses.json", method="clingo", verbose=True)
+        >>> result = query(knowledge="cse_courses.lp", method="clingo", verbose=True, query="cse_reqs.lp.")
         >>> print(result)
         ...
-        >>> result = query(knowledge="cse_courses.json", method="ergoai", query="?X[ancestor->?Y].")
+        >>> result = query(knowledge="cse_courses.ergo", method="ergoai", query="?X[Credits->3].")
         >>> print(result)
         ...
 
@@ -43,13 +43,13 @@ def query(
         num_models: Number of models to generate for Clingo queries. Defaults to None.
         configuration: Configuration for Clingo queries. Options are "handy" or "competition". Defaults to "handy".
         parallel_mode: Parallel mode, maximum number of threads. Defaults to None.
-        query: Query to be passed to ErgoAI. Defaults to None.
+        query: Query or query file paths to be passed to Clingo or ErgoAI. Clingo can take multiple input files as a tuple of strings. ErgoAI can only take string inputs. Input must be a string or a tuple of strings. Defaults to None.
 
     Raises:
-        ValueError: _description_
+        ValueError: If the input method is not supported.
 
     Returns:
-        _description_
+        Results of the query.
     """
     if method.lower() == "clingo":
         result: str = query_clingo(
@@ -58,6 +58,7 @@ def query(
             num_models=num_models,
             configuration=configuration,
             parallel_mode=parallel_mode,
+            query=query,
         )
     elif (method.lower() == "ergoai") or (method.lower() == "ergo"):
         result: str = query_ergoai(knowledge=knowledge, query=query)
